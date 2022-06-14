f60f797af2b25039851543f10423a322bbf8ad06
LiamsG <liamsi999@outlook.com>
2022-06-14 12:04:00 +0200

    ..

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+void  ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+
+
+void  ft_print_comb2(void)
+{
+        int i;
+
+        int j;
+
+        i = 0;
+
+        while ( i <= 99 )
+        {
+                j = i + 1;
+
+                while ( j <= 99 )
+                {
+
+                  ft_putchar(i/10+'0');
+                  ft_putchar(i%10+'0');
+                  
+                  ft_putchar(' ');
+                  
+                  ft_putchar(j/10+'0');
+                  ft_putchar(j%10+'0');
+                  
+                  ft_putchar(',');
+
+		  ft_putchar(' ');
+                  
+                  j++;
+                }
+                i++;
+        }
+}
